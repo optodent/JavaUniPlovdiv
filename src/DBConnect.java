3a6c834934f7a64b0d35229dfa766cf4ec3c3a4b
Stabbz <d.damianow@gmail.com>
2015-01-14 20:30:22 +0200

    osu6testvena konekciq + metodi za obrabotka na baza danni

--- src/DBConnect.java
+++ src/DBConnect.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -19,25 +20,89 @@ public class DBConnect {
 		}catch(Exception exe){
 			System.out.println("Error" + exe);
 		}
-	}
+	}// end DBConnect
 	
+		
+	// metod za dobavqne na kniga koito priema String ime na knigata i koli4estvo na knigata
+	public void addBook(String ime, int qty) {
+		
+		try{					
+			String query =  " insert into knigi (kname, qty)"
+			        + " values (?, ?)";
+			PreparedStatement pst = con.prepareStatement(query);	
+			pst.setString(1, ime);
+			pst.setInt(2, qty);
+			
+			pst.execute();
+					
+		}catch(Exception ex){
+			System.out.println(ex);
+		}
+	}// end addBook
+	
+	// metod za suzdavane na tablicite
+	public void setTables() {
+		try{
+			String query3 = "CREATE TABLE inforeg" +
+		                   "(id INTEGER not NULL AUTO_INCREMENT, " +
+		                   " personid INT , " + 
+		                   " knigaid INT, " +
+		                   " vzeta DATE, " +
+		                   " vurnata DATE, " +
+		                   " FOREIGN KEY ( personid ) REFERENCES person(id), " +
+		                   " FOREIGN KEY ( knigaid ) REFERENCES knigi(id), " +
+		                   " PRIMARY KEY ( id ))";
+			
+			String query1 = "CREATE TABLE person" +
+	                   "(id INTEGER not NULL AUTO_INCREMENT, " +
+	                   " fname VARCHAR(20) NOT NULL, " + 
+	                   " lname VARCHAR(20) NOT NULL, " +
+	                   " faknum VARCHAR(10) NOT NULL, " +
+	                   " specialnost INT, " +
+	                   " FOREIGN KEY ( specialnost ) REFERENCES specialnosti(id), " +	                
+	                   " PRIMARY KEY ( id ))";
+			
+			String query = "CREATE TABLE specialnosti" +
+	                   "(id INTEGER not NULL AUTO_INCREMENT, " +
+	                   " sname VARCHAR(20) NOT NULL, " + 	                   
+	                   " PRIMARY KEY ( id ))";
+			
+			String query2 = "CREATE TABLE knigi" +
+	                   "(id INTEGER not NULL AUTO_INCREMENT, " +
+	                   " kname VARCHAR(30) NOT NULL, " + 
+	                   " qty INT NOT NULL, " +	                  	                  	                   
+	                   " PRIMARY KEY ( id ))";
+	                   
+			st.execute(query);
+			st.execute(query1);
+			st.execute(query2);
+			st.execute(query3);
+			
+			
+		}catch(Exception ex){
+			System.out.println(ex);
+		}
+		
+	}// end setTables
+	
+	// metod za 4etene na informaciq
 	public void getData() {
-		  try{
-		   
-		   String query = "select * from tutorials_tbl";
-		   rs = st.executeQuery(query);
-		   System.out.println("Records from Database");
-		   while(rs.next()){
-		    String title = rs.getString("tutorial_title");
-		    String author = rs.getString("tutorial_author");
-		    String date = rs.getString("submission_date");
-		    
-		    System.out.println("Title: " + title + "    "+"Name: "+author+"   "+"Date: "+date);
-		    
-		   }
-		   
-		  }catch(Exception ex){
-		   System.out.println(ex);
-		  }
-		 }
-}
+		try{
+			
+			String query = "select * from knigi";
+			rs = st.executeQuery(query);
+			System.out.println("Records from Database");
+			while(rs.next()){
+				String name = rs.getString("kname");
+				int qty = rs.getInt("qty");
+				
+				
+				System.out.println("Bla: "+ name + qty);
+				
+			}
+			
+		}catch(Exception ex){
+			System.out.println(ex);
+		}
+	}// end getData()
+}// end class
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		DBConnect connect = new DBConnect();
+		// connect.setTables();
+		// connect.addBook("topki", 33);
 		connect.getData();
 	}
 
