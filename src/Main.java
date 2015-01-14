2baf744054b88dcedabe8ca360167b43765fb12d
Ivelin Rusev <optodent93@gmail.com>
2015-01-14 19:52:16 +0200

    tab issues fixed

--- src/Main.java
+++ src/Main.java
@@ -74,37 +74,12 @@ public class Main extends JFrame{
             return null;
         }
     }
-     
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from
-     * the event dispatch thread.
-     */
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("TabbedPaneDemo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         
-        //Add content to the window.
-        frame.add(new Main(), BorderLayout.CENTER);
-         
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
-
+   
 	public static void main(String[] args) {
 		DBConnect connect = new DBConnect();
-		connect.getData();
-		//Schedule a job for the event dispatch thread:
-        //creating and showing this application's GUI.
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                //Turn off metal's use of bold fonts
-        UIManager.put("swing.boldMetal", Boolean.FALSE);
-        createAndShowGUI();
-            }
-        });
+		Main main = new Main();
+		
+      
 	}
 
 }
